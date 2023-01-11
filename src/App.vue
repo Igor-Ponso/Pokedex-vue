16ac79e505f1d55affba5a53006a89c83c37aaac
Igor Ponso <igorponso07@gmail.com>
2023-01-11 01:38:26 -0300

    feat: create card and async functions for axios pokeapi

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,41 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+  import { reactive, onMounted } from 'vue';
+  import Cards from '@/components/Cards.vue';
+  import axios from 'axios';
+  import { Pokemon } from '@/interfaces/Pokemon';
+
+  //const pokemons = ref([] as Pokemon[]);
+  //const pokemons = ref<Pokemon[]>([]); // * Other way for the same thing
+
+  const pokemonData = reactive([] as Pokemon[]);
+
+  const api = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2',
+  });
+
+  const fetchPokemon = async () => {
+    for (let i = 1; i <= 151; i++) {
+      await api.get('/pokemon/' + i).then((response) => {
+        pokemonData.push(response.data);
+      });
+    }
+  };
+  onMounted(fetchPokemon);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1>{{ $t('greetings') }}</h1>
+  <div id="card-area">
+    <template v-for="pokemon in pokemonData" :key="pokemon.id">
+      <Cards :pokemon="pokemon" />
+    </template>
   </div>
-  <HelloWorld />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="stylus" scoped>
+  #card-area
+    display inline-flex
+    flex-wrap wrap
+    justify-content center
+    gap: 3rem;
 </style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+  import { Pokemon } from '@/interfaces/Pokemon';
+
+  const props = defineProps<{
+    pokemon: Pokemon;
+  }>();
+
+  const pokemonGifPath = (id: number) => {
+    return `/pokedex-vue/src/assets/svgs/pokemons/${id}.svg`;
+  };
+
+  const pokemonTypePath = (type_name: string) => {
+    return `/pokedex-vue/src/assets/svgs/type/${type_name}.svg`;
+  };
+
+  const checkTypes = (types: number) => {
+    if (types === 2) {
+      return {
+        background: `linear-gradient(var(--color-${props.pokemon.types[0].type.name}), 
+        var(--color-${props.pokemon.types[1].type.name}))`,
+      };
+    } else {
+      return {
+        background: `linear-gradient(var(--color-${props.pokemon.types[0].type.name}), 
+        var(--color-${props.pokemon.types[0].type.name}))`,
+      };
+    }
+  };
+</script>
+
+<template>
+  <div class="card" :style="checkTypes(props.pokemon.types.length)">
+    <div class="card-header">
+      <span>{{ props.pokemon.id }}</span>
+      <button>Shiny</button>
+    </div>
+    <img :src="pokemonGifPath(pokemon.id)" alt="" class="card-image" />
+
+    <div class="pokemon_data">
+      {{ pokemon.name }}
+      <div class="pokemon_type">
+        <div
+          v-for="item in props.pokemon.types"
+          :key="item.type.url"
+          class="card-type"
+          :style="{ background: `var(--color-type-${item.type.name})` }"
+        >
+          <img
+            :src="pokemonTypePath(item.type.name)"
+            alt=""
+            class="card-type-item"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="stylus">
+
+  .card
+    height 17.5rem
+    width 12.5rem
+    border-radius 1rem
+    padding 1rem
+    display flex
+    flex-direction column
+    justify-content space-between;
+    color #252a41
+    text-transform: capitalize;
+  .card-header
+    display flex
+    justify-content space-between
+
+  .card-image
+    height: 150px;
+
+  .card-type
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    background red
+    display: -webkit-flex;
+    display: flex;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-align-content: center;
+    align-content: center;
+    -webkit-align-items: center;
+    align-items: center;
+
+  .card-type-item
+    width: 20px;
+    height: 20px;
+
+  .pokemon_data
+    display flex
+    flex-direction column
+    justify-content space-evenly;
+    gap: 1.25rem;
+
+  .pokemon_type
+    display flex
+    grid-gap: 0 10px;
+    grid-gap: 0 20px;
+    gap: 0 20px;
+    -webkit-justify-content: center;
+    justify-content: center;
+</style>
