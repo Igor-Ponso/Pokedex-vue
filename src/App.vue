7e8d2461c82761cc09c8490b9e3cf073b813e7e7
Igor Ponso <igorponso@Igors-MacBook-Pro.local>
2023-01-15 09:36:43 -0300

    refactor: refactor app.vue for use store and service + toast

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,21 @@
 <script setup lang="ts">
-  import { reactive, onMounted } from 'vue';
+  import { onMounted } from 'vue';
   import Cards from '@/components/Cards.vue';
-  import axios from 'axios';
-  import { Pokemon } from '@/interfaces/Pokemon';
+  import { usePokemonStore } from '@/stores/Pokemon';
+  import { fetchPokemonsData } from './services/fetchPokemons';
 
-  //const pokemons = ref([] as Pokemon[]);
-  //const pokemons = ref<Pokemon[]>([]); // * Other way for the same thing
+  const pokemonsData = usePokemonStore().pokemonList;
 
-  const pokemonData = reactive([] as Pokemon[]);
-
-  const api = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2',
-  });
-
-  const fetchPokemon = async () => {
-    for (let i = 1; i <= 151; i++) {
-      await api.get('/pokemon/' + i).then((response) => {
-        pokemonData.push(response.data);
-      });
-    }
-  };
-
-  const imageUrl = {
-    path: '../src/assets/svgs/pokemons/4.svg',
-  };
-
-  onMounted(fetchPokemon);
+  onMounted(fetchPokemonsData);
 </script>
 
 <template>
   <h1>{{ $t('greetings') }}</h1>
   <div id="card-area">
-    <template v-for="pokemon in pokemonData" :key="pokemon.id">
+    <template v-for="pokemon in pokemonsData" :key="pokemon.id">
       <Cards :pokemon="pokemon" />
     </template>
   </div>
-  <img :src="imageUrl.path" alt="" />
-  oi
 </template>
 
 <style lang="stylus" scoped>
