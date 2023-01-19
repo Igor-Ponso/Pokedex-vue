ee31de009ad95907060fa2e8032ed7982d38bf6f
Igor Ponso <igorponso07@gmail.com>
2023-01-18 23:27:36 -0300

    build: add ohvueicon package

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createI18n } from 'vue-i18n';
 import Toast from 'vue-toastification';
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { BiStars } from "oh-vue-icons/icons";
+
 
 import '@/style.css';
 import '@/assets/stylus/__colors.styl';
 import '@/assets/stylus/__spacing.styl';
 import 'vue-toastification/dist/index.css';
+
+addIcons(BiStars);
+
 
 import en from './locales/en.json';
 
@@ -19,9 +25,10 @@ const i18n = createI18n({
     en: en,
   },
 });
+
 const pinia = createPinia();
 const app = createApp(App);
-
+app.component("v-icon", OhVueIcon);
 app.use(i18n);
 app.use(Toast);
 app.use(pinia);
