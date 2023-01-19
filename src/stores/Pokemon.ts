6fff21fa2f7f67675f4f0605d1c25c2777b4ca5d
Igor Ponso <igorponso07@gmail.com>
2023-01-18 23:27:01 -0300

    refactor: create new pokemon interface and sub interfaces

--- src/stores/Pokemon.ts
+++ src/stores/Pokemon.ts
@@ -1,11 +1,11 @@
 import { PokemonEntries } from '@/interfaces/PokemonEntries';
-import { reactive, ref } from 'vue';
+import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 import { Pokemon } from '@/interfaces/Pokemon';
 
 export const usePokemonStore = defineStore('pokemons', () => {
   const pokemonList = reactive([] as Pokemon[]);
-  const pokemonEntries = ref<PokemonEntries>();
+  const pokemonEntries = reactive([] as PokemonEntries[]);
 
   return { pokemonList, pokemonEntries };
 });
